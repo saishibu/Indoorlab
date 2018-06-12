1744fc3f83752691ec5a6264a2c8880d3f2e10ff
Sai Shibu <saishibu38380@gmail.com>
2018-06-12 16:16:14 +0530

    Updated L1 Algorithm

---
+++ algo.py
@@ -0,0 +1,17 @@
+#Level 1 Smart grid algorithms
+#Build: 2018-06-12-V1
+
+#Current Check
+#Voltage Check
+#Frequency Check
+#PF Check
+#Power check (W)
+def threshold(A,th):
+  if A>th:
+    B=1
+  else:
+    B=0
+  return B
+   
+
+
